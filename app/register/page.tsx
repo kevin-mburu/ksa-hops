b6eb2077beaf4dd93df640f5567a0126d2ad650c
kevin-mburu <kevinmburumungai@gmail.com>
2026-04-25 02:06:30 +0300

    Update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import dynamic from 'next/dynamic'
 const RegisterApp = dynamic(() => import('./RegisterApp'), { ssr: false })
 export default function RegisterPage() { return <RegisterApp /> }
